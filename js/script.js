41c9681fe2c96606473c3433845ceec790300ff9
jefrinjui <jefrin.jui.dev@gmail.com>
2024-10-01 19:21:06 +0600

    js comlpete

--- js/script.js
+++ js/script.js
@@ -2,11 +2,6 @@ let input =document.querySelector(".mainInput")
 let button =document.querySelector(".maniButton")
 let result =document.querySelector(".result")
 
-// button.addEventListener('click' , ()=>{
-//     console.log(input.value)
-//     result.innerHTML=input.value 
-    
-// })
 button.addEventListener('click' , ()=>{
     console.log(input.value)
     result.innerHTML=input.value
