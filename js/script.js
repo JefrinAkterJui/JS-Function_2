a13e79238949cc17db1e066867133c2feb32c41c
jefrinjui <jefrin.jui.dev@gmail.com>
2024-10-01 18:50:38 +0600

    JS complete

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let input =document.querySelector(".mainInput")
+let button =document.querySelector(".maniButton")
+let result =document.querySelector(".result")
+
+// button.addEventListener('click' , ()=>{
+//     console.log(input.value)
+//     result.innerHTML=input.value 
+    
+// })
+button.addEventListener('click' , ()=>{
+    console.log(input.value)
+    result.innerHTML=input.value
+})
